ed04c03f1bb198de2c80854ab089bfb21bdb1964
vasili-serchev <vasili.serchev@protonmail.com>
2022-07-29 18:00:38 -0700

    minting of ZCHF

--- scripts/maintenance/mintCHF.ts
+++ scripts/maintenance/mintCHF.ts
@@ -2,15 +2,18 @@
 /*
     Script to mint moch XCHF
     1) set private key in terminal without 0x, via export PK="123cafefefefefeCACe..."
+        minting will be done for that wallet
     2) edit script
-    4) ts-node scripts/maintenance/mintXCHF.ts
+    4) ts-node scripts/maintenance/mintCHF.ts
 */
 const ethers = require("ethers");
 import { SigningKey } from "@ethersproject/signing-key";
 import { floatToDec18 } from "../math";
 const NODE_URL = "https://rpc.sepolia.org";
 const ERC20_ABI = require('../../abi/MockXCHFToken.json');
-
+const BRIDGE_ABI = require('../../abi/StablecoinBridge.json');
+const mockXCHFAddr = "0x4bfc8c42362C7626D0555cA273Cc12c1580E8A2d";
+const bridgeAddr = "0xF8e20518393FF2B8Db6C00adBAc2c48956041c41";
 let pk: string | SigningKey = <string>process.env.PK;
 
 export async function getSigningManagerFromPK(ctrAddr, ctrAbi, nodeUrl, pk) {
@@ -21,16 +24,26 @@ export async function getSigningManagerFromPK(ctrAddr, ctrAbi, nodeUrl, pk) {
     return signingContractManager;
 }
 
-async function mint(address, amount : number) {
-    const mockXCHFAddr = "0x4bfc8c42362C7626D0555cA273Cc12c1580E8A2d";
+async function mintXCHF(amount : number) {
+    const wallet = new ethers.Wallet(pk);
+    let address = wallet.address;
     let tokenContract = await getSigningManagerFromPK(mockXCHFAddr, ERC20_ABI, NODE_URL, pk);
     await tokenContract.mint(address, floatToDec18(amount));
 }
 
+async function mintZCHF(amount : number) {
+    let dAmount = floatToDec18(amount);
+    let tokenContract = await getSigningManagerFromPK(mockXCHFAddr, ERC20_ABI, NODE_URL, pk);
+    let bridgeContract = await getSigningManagerFromPK(bridgeAddr, BRIDGE_ABI, NODE_URL, pk);
+    await tokenContract.approve(bridgeAddr, dAmount);
+    let tx = await bridgeContract["mint(uint256)"](dAmount);
+    console.log("minted XCHF:", tx.hash);
+}
+
 async function start() {
-    let amount = 1000;
-    let mintForAddress = "0x0aB6527027EcFF1144dEc3d78154fce309ac838c";
-    await mint(mintForAddress, amount);
+    let amount = 500;
+    //await mintXCHF(mintForAddress, amount);
+    await mintZCHF(amount);
 }
 
 start();
