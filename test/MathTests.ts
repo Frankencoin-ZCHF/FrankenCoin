828d39b0b998bd101cf9669c1e274e55579628e3
vasili-serchev <vasili.serchev@protonmail.com>
2022-09-10 17:21:34 -0400

    test more numbers for cubic root

--- test/MathTests.ts
+++ test/MathTests.ts
@@ -67,18 +67,22 @@ describe("Math Tests", () => {
             
         });
         it("cubic root", async () => {
-            let number = 1.5;
-            let result = 1.5**(1/3);
-            let fNumber = floatToDec18(number);
-            let fResult = await MathContract.cubicRoot(fNumber);
-            let resultRec = dec18ToFloat(fResult);
-            let err = Math.abs(result-resultRec);
-            if (err>1e-6) {
-                console.log("expected=", result);
-                console.log("received=", resultRec);
-                console.log("abs error=", err);
+            let numbers = [0.01, 0.9, 1, 1.5, 2, 10];
+            for(var k = 0; k<numbers.length; k++) {
+                let number = numbers[k];
+                let result = number**(1/3);
+                let fNumber = floatToDec18(number);
+                let fResult = await MathContract.cubicRoot(fNumber);
+                let resultRec = dec18ToFloat(fResult);
+                let err = Math.abs(result-resultRec);
+                if (err>1e-6) {
+                    console.log("number =", result);
+                    console.log("expected=", result);
+                    console.log("received=", resultRec);
+                    console.log("abs error=", err);
+                }
+                expect(err).to.be.lessThan(1e-6);
             }
-            expect(err).to.be.lessThan(1e-6);
         });
     });
 
