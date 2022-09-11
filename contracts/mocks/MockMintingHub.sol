c995dbbb749eff148aad0e36c85b3f7d44ab771a
vasili-serchev <vasili.serchev@protonmail.com>
2022-09-10 22:33:35 -0400

    mock minter for ease of testing

---
+++ contracts/mocks/MockMintingHub.sol
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "../MintingHub.sol";
+
+// this minting hub stores the address of
+// the last position created. Otherwise cumbersome to
+// access through hardhat/ethers
+contract MockMintingHub is MintingHub {
+
+    address public lastPositionAddress;
+    constructor(address _zchf, address _factory) MintingHub (_zchf, _factory){}
+
+    function openPositionWithAddress(address _collateral, uint256 _minCollateral, 
+        uint256 _initialCollateral, uint256 _initialLimit, 
+        uint256 _duration, uint32 _fees, uint32 _reserve) 
+        public returns (address) 
+    {
+        lastPositionAddress = openPosition(_collateral, _minCollateral,_initialCollateral,
+            _initialLimit, _duration, _fees, _reserve);
+        return lastPositionAddress;
+    }
+
+}
