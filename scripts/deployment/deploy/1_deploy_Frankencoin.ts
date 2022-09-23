06e8227090e3f095eeacffca9db5e719909da989
vasili-serchev <vasili.serchev@protonmail.com>
2022-09-23 15:24:47 -0400

    deployment scripts for Frankencoin with param

--- scripts/deployment/deploy/1_deploy_Frankencoin.ts
+++ scripts/deployment/deploy/1_deploy_Frankencoin.ts
@@ -9,8 +9,18 @@ import {deployContract, ZERO_ADDRESS} from "../deployUtils";
     npx hardhat deploy --network sepolia
 */
 const deploy: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
-
-    await deployContract(hre, "Frankencoin");
+    const paramFile = "params.json";
+    let chainId = hre.network.config["chainId"];
+    let paramsArr = require(__dirname + `/parameters/${paramFile}`);
+    // find config for current chain
+    for(var k=0; k<paramsArr.length && paramsArr[k].chainId!=chainId; k++);
+    let params = paramsArr[k];
+    if (chainId!=params.chainId) {
+        throw new Error("ChainId doesn't match");
+    }
+    let minApplicationPeriod=params['minApplicationPeriod'];
+    console.log("Min application period =", minApplicationPeriod);
+    await deployContract(hre, "Frankencoin", [minApplicationPeriod]);
     
 };
 export default deploy;
