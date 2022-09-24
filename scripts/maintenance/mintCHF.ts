0ee2da82da1af885e2f0237291275fd9e2d7a0de
vasili-serchev <vasili.serchev@protonmail.com>
2022-09-24 14:18:32 -0400

    mint mock token

--- scripts/maintenance/mintCHF.ts
+++ scripts/maintenance/mintCHF.ts
@@ -12,6 +12,7 @@ const NODE_URL = "https://rpc.sepolia.org";
 const ERC20_ABI = require('../../abi/MockXCHFToken.json');
 const BRIDGE_ABI = require('../../abi/StablecoinBridge.json');
 const mockXCHFAddr = "0x081AEb4c123DF59a31890E038A1cCCAa32F41616";
+const mockVOLTknAddr = "0xf18b58536ab9c71e54ED2eF4485B3f2cDaD3C172";
 const bridgeAddr = "0x8f8dE84f9bE411e080A77512cC50CF8A3a1051c4";
 let pk: string | SigningKey = <string>process.env.PK;
 
@@ -30,6 +31,12 @@ async function mintXCHF(amount : number, address : string) {
     await tokenContract.mint(address, floatToDec18(amount));
 }
 
+async function mintMockVOLToken(amount : number, address : string) {
+    const wallet = new ethers.Wallet(pk);
+    let tokenContract = await getSigningManagerFromPK(mockVOLTknAddr, ERC20_ABI, NODE_URL, pk);
+    await tokenContract.mint(address, floatToDec18(amount));
+}
+
 async function mintZCHF(amount : number) {
     let dAmount = floatToDec18(amount);
     let tokenContract = await getSigningManagerFromPK(mockXCHFAddr, ERC20_ABI, NODE_URL, pk);
@@ -40,11 +47,13 @@ async function mintZCHF(amount : number) {
 }
 
 async function start() {
-    let amount = 5_000;
-    //let address = wallet.address;
+    let amount = 10;
+    const wallet = new ethers.Wallet(pk);
+    let address = wallet.address;
     //let address = "0x71C696acd63979B39B8eD5b7a8030c46f34Da716"; // manu
+    //await mintMockVOLToken(amount, address);
     //await mintXCHF(amount, address);
-    await mintZCHF(amount);
+    //await mintZCHF(amount);
 }
 
 start();
