ed7c6bbc6cde6c77fce4673ca4e8652f56114310
vasili-serchev <vasili.serchev@protonmail.com>
2022-09-24 15:46:55 -0400

    details

--- scripts/maintenance/mintCHF.ts
+++ scripts/maintenance/mintCHF.ts
@@ -2,7 +2,7 @@
     Script to mint mock XCHF
     1) set private key in terminal without 0x, via export PK="123cafefefefefeCACe..."
         minting will be done for that wallet
-    2) edit script
+    2) edit script (addresses correct?)
     4) ts-node scripts/maintenance/mintCHF.ts
 */
 const ethers = require("ethers");
@@ -11,9 +11,9 @@ import { floatToDec18 } from "../math";
 const NODE_URL = "https://rpc.sepolia.org";
 const ERC20_ABI = require('../../abi/MockXCHFToken.json');
 const BRIDGE_ABI = require('../../abi/StablecoinBridge.json');
-const mockXCHFAddr = "0x081AEb4c123DF59a31890E038A1cCCAa32F41616";
-const mockVOLTknAddr = "0xf18b58536ab9c71e54ED2eF4485B3f2cDaD3C172";
-const bridgeAddr = "0x8f8dE84f9bE411e080A77512cC50CF8A3a1051c4";
+const mockXCHFAddr = "0x20Ab5e22C812b51F29ADb70b467896f5338C7b97";
+const mockVOLTknAddr = "0x894E14D5a7DDA4175E2A9EFee4B3bED6Dd930a98";
+const bridgeAddr = "0x85C0CCbd95530e5d591d42E5C53302dcC97B180e";
 let pk: string | SigningKey = <string>process.env.PK;
 
 export async function getSigningManagerFromPK(ctrAddr, ctrAbi, nodeUrl, pk) {
@@ -47,10 +47,11 @@ async function mintZCHF(amount : number) {
 }
 
 async function start() {
-    let amount = 10;
+    let amount = 10_000;
     const wallet = new ethers.Wallet(pk);
     let address = wallet.address;
     //let address = "0x71C696acd63979B39B8eD5b7a8030c46f34Da716"; // manu
+    console.log("minting for ", address);
     //await mintMockVOLToken(amount, address);
     //await mintXCHF(amount, address);
     //await mintZCHF(amount);
