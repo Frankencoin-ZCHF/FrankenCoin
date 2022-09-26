6622877f010535b64c8f13a61ea99d3bdea925cb
vasili-serchev <vasili.serchev@protonmail.com>
2022-09-25 20:33:28 -0400

    fix event

--- contracts/MintingHub.sol
+++ contracts/MintingHub.sol
@@ -22,7 +22,7 @@ contract MintingHub {
     IFrankencoin public immutable zchf; // currency
     Challenge[] public challenges;
     
-    event PositionOpened(address indexed hub, address indexed owner, 
+    event PositionOpened(address indexed owner, 
         address collateral, uint256 initialCollateral, uint256 initialLimit, 
         uint256 duration, uint256 challengePeriod, uint32 fees, uint32 reserve, address positionAddr);
 
@@ -72,8 +72,8 @@ contract MintingHub {
         zchf.transferFrom(msg.sender, address(zchf.reserve()), OPENING_FEE);
         IERC20(_collateral).transferFrom(msg.sender, address(pos), _initialCollateral);
 
-        emit PositionOpened(address(this), msg.sender, _collateral, _initialCollateral, 
-            _initialLimit, _duration, _challengePeriod, _fees, _reserve, address(this));
+        emit PositionOpened(msg.sender, _collateral, _initialCollateral, 
+            _initialLimit, _duration, _challengePeriod, _fees, _reserve, address(pos));
 
         return address(pos);
     }
