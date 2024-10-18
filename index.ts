ed62c03cb4fef7293fa3c7421c17b665044c1238
samclassix <samclassix@proton.me>
2024-10-18 13:48:01 +0200

    feat: npm module and correct export with tsup

--- index.ts
+++ index.ts
@@ -1,27 +1,27 @@
 // chain addresses and helpers
-export * from './exports/address.config.ts';
+export * from './exports/address.config';
 
 // abi exports
-export * from './exports/abis/Frankencoin.ts';
-export * from './exports/abis/Equity.ts';
-export * from './exports/abis/StablecoinBridge.ts';
-export * from './exports/abis/FPSWrapper.ts';
-export * from './exports/abis/FPSUnlock.ts';
+export * from './exports/abis/Frankencoin';
+export * from './exports/abis/Equity';
+export * from './exports/abis/StablecoinBridge';
+export * from './exports/abis/FPSWrapper';
+export * from './exports/abis/FPSUnlock';
 
-export * from './exports/abis/MintingHubV1.ts';
-export * from './exports/abis/PositionV1.ts';
+export * from './exports/abis/MintingHubV1';
+export * from './exports/abis/PositionV1';
 
-export * from './exports/abis/MintingHubV2.ts';
-export * from './exports/abis/PositionV2.ts';
-export * from './exports/abis/PositionRoller.ts';
-export * from './exports/abis/Leadrate.ts';
-export * from './exports/abis/Savings.ts';
+export * from './exports/abis/MintingHubV2';
+export * from './exports/abis/PositionV2';
+export * from './exports/abis/PositionRoller';
+export * from './exports/abis/Leadrate';
+export * from './exports/abis/Savings';
 
-export * from './exports/abis/IERC20.ts';
-export * from './exports/abis/ERC20.ts';
-export * from './exports/abis/ERC20PermitLight.ts';
-export * from './exports/abis/Ownable.ts';
-export * from './exports/abis/UniswapV3Pool.ts';
+export * from './exports/abis/IERC20';
+export * from './exports/abis/ERC20';
+export * from './exports/abis/ERC20PermitLight';
+export * from './exports/abis/Ownable';
+export * from './exports/abis/UniswapV3Pool';
 
 // constructor args
-// export * from './ignition/constructor-args/index.ts';
+// export * from './ignition/constructor-args/index';
