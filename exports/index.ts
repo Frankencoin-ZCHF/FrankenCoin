b0a19af2f329544e2c1787853079b5fa5122bdd2
samclassix <96245183+samclassix@users.noreply.github.com>
2024-11-26 11:36:37 +0100

    Npm support (#39)

---
+++ exports/index.ts
@@ -0,0 +1,17 @@
+// chain addresses and helpers
+export * from "./address.config";
+
+// abi exports
+export * from "./abis/core/EuroCoin";
+export * from "./abis/core/Equity";
+
+export * from "./abis/utils/ERC20";
+export * from "./abis/utils/ERC20PermitLight";
+export * from "./abis/utils/Ownable";
+export * from "./abis/utils/UniswapV3Pool";
+export * from "./abis/utils/StablecoinBridge";
+export * from "./abis/utils/DEPSWrapper";
+// export * from './abis/utils/FPSUnlock';
+
+// constructor args
+// export * from './ignition/constructor-args/index';
