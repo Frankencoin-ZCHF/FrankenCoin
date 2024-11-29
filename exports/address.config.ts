76cb380bf61341b9eef1a2fe49ec321c909614a2
Patrick <42653152+lapatric@users.noreply.github.com>
2024-11-29 12:42:46 +0100

    Prepare files for NPM package (#50)
    * update exports for npm packages
    
    * add Leadrate ABI to /exports
    
    * format typescript ABIs
    
    * cleanup
    
    * formatting

--- exports/address.config.ts
+++ exports/address.config.ts
@@ -2,28 +2,28 @@ import { mainnet, polygon } from "viem/chains";
 import { Address, zeroAddress } from "viem";
 
 export interface ChainAddress {
-  eurocoin: Address;
+  decentralizedEURO: Address;
   equity: Address;
-  deps: Address;
+  DEPSwrapper: Address;
   bridge: Address;
-
-  // accept any optional key
-  [key: string]: Address | undefined;
+  eurt: Address;
+  savings: Address;
+  roller: Address;
+  mintingHubV2: Address;
+  positionFactoryV2: Address;
 }
 
 export const ADDRESS: Record<number, ChainAddress> = {
   [mainnet.id]: {
     // natice contract addresses
-    eurocoin: "0xd45e911843721083A2751fA9Cc9D2a8089D8C0f5",
+    decentralizedEURO: "0xd45e911843721083A2751fA9Cc9D2a8089D8C0f5",
     equity: "0xC92aF56C354FCF641f4567a04fd7032013E8A314",
-    deps: zeroAddress,
-    bridge: zeroAddress,
-  },
-  [polygon.id]: {
-    // For test deployment only
-    eurocoin: "0xdd537c07AAf1Be13b396EB37B2a2E41034002C94",
-    equity: "0x87f562A2ACb54134fD8203911Ca65Ca366062afF",
-    deps: "0x00b589940EE4274129a87428143E6F15a1993743",
-    bridge: zeroAddress,
+    DEPSwrapper: "0xE699A78B3eC44Ab013f1839305bB1a5a987A05B0",
+    bridge: "0x7416E83375C7cb4D74F06F8Dc4Acd4243cEA8248",
+    eurt: "0xC581b735A1688071A1746c968e0798D642EDE491",
+    savings: "0x4Be3ce63952fd4605d4a6FA4C5F877F6AC73bAa2",
+    roller: "0x18fA3796a43c297950f76A1bA6162d58dD2d7EDa",
+    mintingHubV2: "0x762FbFA1f0C62a31FBDd91be63b01Fcd392d733B",
+    positionFactoryV2: "0x9F67F1FF7A22447b04277A214E9BFaF08d832bb1",
   },
 };
--- exports/index.ts
+++ exports/index.ts
@@ -2,12 +2,19 @@
 export * from "./address.config";
 
 // abi exports
-export * from "./abis/core/EuroCoin";
+export * from "./abis/core/DecentralizedEURO";
 export * from "./abis/core/Equity";
+export * from "./abis/core/Savings";
+
+export * from "./abis/MintingHubV2/PositionFactoryV2";
+export * from "./abis/MintingHubV2/MintingHubV2";
+export * from "./abis/MintingHubV2/PositionRoller";
+export * from "./abis/MintingHubV2/PositionV2";
 
 export * from "./abis/utils/ERC20";
 export * from "./abis/utils/ERC20PermitLight";
 export * from "./abis/utils/Ownable";
+export * from "./abis/utils/Leadrate";
 export * from "./abis/utils/UniswapV3Pool";
 export * from "./abis/utils/StablecoinBridge";
 export * from "./abis/utils/DEPSWrapper";
