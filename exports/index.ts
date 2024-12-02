06df820c9df1286eb63dfe857b18ce07c233edee
samclassix <samclassix@proton.me>
2024-12-02 12:08:32 +0100

    feat: npm export with updated test deployment and v2

---
+++ exports/index.ts
@@ -0,0 +1,26 @@
+// chain addresses and helpers
+export * from "./address.config";
+
+// abi exports
+export * from "./abis/core/Frankencoin";
+export * from "./abis/core/Equity";
+export * from "./abis/core/Leadrate";
+export * from "./abis/core/Savings";
+
+export * from "./abis/MintingHubV1/MintingHubV1";
+export * from "./abis/MintingHubV1/PositionFactoryV1";
+export * from "./abis/MintingHubV1/PositionV1";
+
+export * from "./abis/MintingHubV2/MintingHubV2";
+export * from "./abis/MintingHubV2/PositionFactoryV2";
+export * from "./abis/MintingHubV2/PositionV2";
+export * from "./abis/MintingHubV2/PositionRoller";
+
+export * from "./abis/utils/IERC20";
+export * from "./abis/utils/ERC20";
+export * from "./abis/utils/ERC20PermitLight";
+export * from "./abis/utils/Ownable";
+export * from "./abis/utils/UniswapV3Pool";
+export * from "./abis/utils/StablecoinBridge";
+export * from "./abis/utils/FPSWrapper";
+export * from "./abis/utils/FPSUnlock";
