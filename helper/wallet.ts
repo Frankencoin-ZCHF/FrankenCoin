c72b74224d8f17fce26bca89b455ba6a8186f159
samclassix <samclassix@proton.me>
2024-12-09 12:08:30 +0100

    fix: correct path to address index

--- helper/wallet.ts
+++ helper/wallet.ts
@@ -1,17 +1,27 @@
-import { ethers } from 'ethers';
+import { ethers } from "ethers";
 
 export function getChildFromSeed(seed: string, index: number) {
-	return ethers.Wallet.fromPhrase(seed).deriveChild(index);
+  return ethers.HDNodeWallet.fromPhrase(
+    seed,
+    undefined,
+    `m/44'/60'/0'/0/${index}`
+  );
 }
 
 export function getAddressFromChildIndex(seed: string, index: number): string {
-	return ethers.Wallet.fromPhrase(seed).deriveChild(index).address;
+  return getChildFromSeed(seed, index).address;
 }
 
-export function getPublicKeyFromChildIndex(seed: string, index: number): string {
-	return ethers.Wallet.fromPhrase(seed).deriveChild(index).publicKey;
+export function getPublicKeyFromChildIndex(
+  seed: string,
+  index: number
+): string {
+  return getChildFromSeed(seed, index).publicKey;
 }
 
-export function getPrivateKeyFromChildIndex(seed: string, index: number): string {
-	return ethers.Wallet.fromPhrase(seed).deriveChild(index).privateKey;
+export function getPrivateKeyFromChildIndex(
+  seed: string,
+  index: number
+): string {
+  return getChildFromSeed(seed, index).privateKey;
 }
