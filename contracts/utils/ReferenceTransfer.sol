d81d53df3014fa24cc180ea84761c63b609cfd0e
Luzius Meisser <luzius@aktionariat.com>
2025-03-29 08:19:08 +0100

    AutoSave
    Possibility to automatically forward incoming payments into the savings module.

--- contracts/utils/ReferenceTransfer.sol
+++ contracts/utils/ReferenceTransfer.sol
@@ -15,29 +15,60 @@ import "../interface/IERC20.sol";
  */
 contract ReferenceTransfer {
 
-    IERC20 public immutable TOKEN;
+    IERC20 public immutable ZCHF;
+    ISavings public immutable SAVINGS;
 
     uint256 internal constant INFINITY = (1 << 255);
+    uint256 internal constant EMPTY_SETTINGS = 0;
+    uint256 internal constant FORWARD_TO_SAVINGS = 1;
+
+    mapping(address => uint256) settings;
 
     event Transfer(address indexed from, address indexed to, uint256 amount, string ref);
 
     error TransferFromRequiresInfiniteAllowance(address owner, address spender);
 
-    constructor(address token) {
-        TOKEN = IERC20(token);
+    constructor(address token, address savings) {
+        ZCHF = IERC20(token);
+        SAVINGS = ISavings(savings);
     }
 
     function transfer(address recipient, uint256 amount, string calldata ref) public returns (bool) {
         emit Transfer(msg.sender, recipient, amount, ref);
-        return TOKEN.transferFrom(msg.sender, recipient, amount);
+        return executeTransfer(msg.sender, recipient, amount);
     }
 
     function transferFrom(address owner, address recipient, uint256 amount, string calldata ref) public returns (bool) {
         if (TOKEN.allowance(owner, msg.sender) < INFINITY) revert TransferFromRequiresInfiniteAllowance(owner, msg.sender);        
         emit Transfer(owner, recipient, amount, ref);
-        return TOKEN.transferFrom(owner, recipient, amount);
+        return executeTransfer(owner, recipient, amount);
     }
 
+    function setAutoSave(bool enabled) external {
+        settings[msg.sender] = enabled ? FORWARD_TO_SAVINGS : EMPTY_SETTINGS;
+    }
+
+    function hasAutoSave() external view returns(bool){
+        return hasAutoSave(msg.sender);
+    }
+
+    function hasAutoSave(address owner) public view returns(bool){
+        return settings[owner] & FORWARD_TO_SAVINGS == FORWARD_TO_SAVINGS;
+    }
+
+    function executeTransfer(address from, address to, uint256 amount) internal returns (bool) {
+        if (hasAutoSave(to)){
+            TOKEN.transferFrom(from, address(this), amount);
+            SAVINGS.save(to, amount);
+        } else {
+            TOKEN.transferFrom(from, to, amount);
+        }
+    }
+
+}
+
+interface ISavings {
+    function save(address owner, uint192 amount) public;
 }
 
 
